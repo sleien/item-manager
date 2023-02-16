bbbf95c7fa22bf313ed2b3e7835dd33d8be211b4
Noah Schneider <noah.schneider@outlook.com>
2023-02-16 02:51:16 +0100

    updated create db user

--- sql/create_db_user.sql
+++ sql/create_db_user.sql
@@ -1,3 +1,3 @@
-CREATE USER 'item_manager'@'%' IDENTIFIED VIA mysql_native_password USING '***';GRANT USAGE ON *.* TO 'item_manager'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `item\_manager\_%`.* TO 'item_manager'@'%';
+CREATE USER 'item_manager'@'%' IDENTIFIED VIA mysql_native_password USING password("CHANGE_ME");GRANT USAGE ON *.* TO 'item_manager'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `item\_manager\_%`.* TO 'item_manager'@'%';
 
 GRANT ALL PRIVILEGES ON `item_manager`.* TO 'item_manager'@'%' WITH GRANT OPTION;
